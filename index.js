ef96af8820d6af26f4b3f76a7bdef92d721333b3
Sean Goossens <sean.goossens@gmail.com>
2023-03-29 20:14:34 -0600

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello server is running").end();
-  console.log(app);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // Start the server
