94011b887ac6e62b1ef20f1703a048f502846c57
Sean Goossens <sean.goossens@gmail.com>
2023-03-30 17:05:44 -0600

    Security update

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabase = createClient(
-  "https://hvxjdgqkvuzoabdavzbs.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2eGpkZ3FrdnV6b2FiZGF2emJzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MDE1NDQwNCwiZXhwIjoxOTk1NzMwNDA0fQ.jP2V9qTzbHtABbaRfoc56k1X0Dof9qlS5n5AElWR0Ms"
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_AUTH;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const databasePull = function () {
   return supabase
